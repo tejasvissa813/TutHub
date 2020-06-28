621ca46e44784e37c2a035b31145cdd832ca3dd3
Deepak Karjala <deepak.karjala@gmail.com>
2020-06-27 17:45:34 -0700

    added sign up

---
+++ app/src/main/java/com/example/tutorhub/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.tutorhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void perform_action(View v) {
+        TextView tv = findViewById(R.id.teacherLogin);
+
+        tv.setText("Logged In");
+    }
+}
